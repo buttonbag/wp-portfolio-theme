76a8a3ee4980e7255c853b65502261253a573829
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-08-20 19:32:09 -0500

    sidebar moved up

--- portfolio/single.php
+++ portfolio/single.php
@@ -7,42 +7,58 @@
  * @package portfolio
  */
 
-get_header();
+// get_header();
 ?>
 
 <?php $is_post = 'post' === get_post_type();
-if( $is_post ) : get_template_part('content', 'nav'); else :
+	if( !$is_post ) : 
 ?>
 <style>
 	.site-header, nav, .mobile-nav-toggle, #wpadminbar {display: none; visibility: hidden;}
 </style>
 <?php endif; ?>
 
-		<div class="work-page">
+	<div id="main" class="
+	my-5
+		<?php if (!$is_post) : ?>
+			m-3
+		<?php endif;?>">		
+		<div class="container">
+			<div class="row">	
+				<div class="col-lg-12">	
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
+		
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'portfolio' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'portfolio' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
+				if ( $is_post ) : 
+					get_sidebar();
+				endif; 
+
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
+		
+				</div>
+			</div>
 		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-			
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'portfolio' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'portfolio' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
 		?>
 		</div>
+		
+	</div>
 
 <?php
-/* get_sidebar(); */
-if ( $is_post ) :
+if ( $is_post ) : 
 get_footer();
 endif;
