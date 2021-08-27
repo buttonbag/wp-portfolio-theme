fe395baffa9b48de38c0b728d933c0b7fe1b7567
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-08-26 20:30:20 -0500

    blog posts added to blog section

--- portfolio/section-blog.php
+++ portfolio/section-blog.php
@@ -8,7 +8,21 @@
         <div class="row">
 
             <!-- main content -->
-            <div class="col-lg-4 col-md-6 ">
+            <?php $num=1; $loop = new WP_Query( array('post_type' => 'post'/* , 'orderby' => 'post_date', 'order' => 'ASC' */) ); ?>
+            <?php while( $loop->have_posts()) : $loop->the_post();?>
+            
+            <div class="col-lg-4 col-md-6 aos-init aos-animate" data-aos="fade-right" data-aos-easing="ease-in-out" data-aos-delay="0">
+                <h4><?php echo the_title();?></h4>
+                <?php echo the_excerpt(); ?>
+                <a href="<?php echo get_permalink();?>" class="btn btn-outline-secondary btn-sm">
+                Continue...</a>
+            </div>
+                    
+            <?php endWhile; ?>
+            <?php wp_reset_query(); ?>
+            <!-- end main content -->
+
+            <!-- <div class="col-lg-4 col-md-6 ">
                 <ul>
                     <?php $num=1; $loop = new WP_Query( array('post_type' => 'post'/* , 'orderby' => 'post_date', 'order' => 'ASC' */) ); ?>
                     <?php while( $loop->have_posts()) : $loop->the_post();?>
@@ -21,7 +35,8 @@
                     <?php endWhile; ?>
                     <?php wp_reset_query(); ?>
                 </ul>
-            </div><!-- end main content -->
+            </div> -->
+            <!-- end main content -->
 
         </div> <!-- end row -->
 
