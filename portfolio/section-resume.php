01a90d5ba690ff55423869396ffebe349571ed9d
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-09-21 11:04:03 -0500

    converted resume to ACF

--- portfolio/section-resume.php
+++ portfolio/section-resume.php
@@ -9,30 +9,16 @@
 
     <div class="row">
       <div class="col-lg-12">
-        <h3 class="resume-title">Professional Experience</h3>
+
+        <?php $loop = new WP_Query( array('post_type' => 'resume_entries', 'meta_key' => 'order_number', 'order' => 'DSC') );
+        while ($loop -> have_posts()) : $loop->the_post(); ?>
+
         <div class="resume-item" data-aos="fade-up" data-aos-delay="100">
-          <h4>FRONT END DEVELOPER + Digital Media Production</h4>
-          <h5>2015 - Present</h5>
-          <p><em>Ogilvy, Chicago, IL</em></p>
-          <ul>
-            <li>Worked closely with designers to ensure products were built according to agency standards that resulted in beating seemingly impossible deadlines.</li>
-            <li>Formulated efficient solutions for a variety of challenges in large volumes.</li>
-            <li>Performed QA of all graphic materials in order to ensure quality and accuracy of the design.</li>
-            <li>Lead skill share sessions to update team skillsets.</li>
-          </ul>
-        </div>
-        <div class="resume-item" data-aos="fade-up" data-aos-delay="100">
-          <h4>Freelance Designer + Developer</h4>
-          <h5>2014 - 2015</h5>
-          <p><em>Staples/Quill, Lincolnshire, IL</em></p>
-          <ul>
-            <li>As a developer, wrote HTML landing pages and also managed Wordpress posts. </li>
-            <li>As a designer, collaborated with the email marketing team to redesign email assets and introduced responsive email templates to the team.</li>
-            <li>Managed Wordpress postings and designed social media graphics.</li>
-          </ul>
-        </div>
-        <div class="resume-item" data-aos="fade-up" data-aos-delay="100">
-          <h4>Lead Designer + Developer</h4>
+          <h4><?php echo the_title();?></h4>
+          <h5><?php the_field('years_active');?></h5>
+          <p><em><?php the_field('company_name');?>, <?php the_field('location');?></em></p>
+          <?php the_field('overview');?>
+          <!-- <h4>Lead Designer + Developer</h4>
           <h5>2010 - 2014</h5>
           <p><em>Media Made Great, Chicago, IL</em></p>
           <ul>
@@ -42,8 +28,10 @@
               collaterals (banner ads, promotional emails, websites, wireframes, storyboards).</li>
             <li>Managed up to 5 projects or tasks at a given time while under pressure.</li>
             <li>Consulted with clients and managers on the most appropriate technologies for each project.</li>
-          </ul>
+          </ul> -->
         </div>
+        <?php endwhile; ?>
+        <?php wp_reset_query(); ?>
       </div>
     </div>
 
